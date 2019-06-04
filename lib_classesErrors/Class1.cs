bd1474db4c228718268d5cf8a8b1a57906de5e94
ex.foms <ex.foms@gmail.com>
2019-06-05 08:33:15 +0900

    Add project files.

---
+++ lib_classesErrors/Class1.cs
@@ -0,0 +1,33 @@
+﻿// Энумератор ошибок глобальный 
+public class clsErrors
+{
+    public enum GateError
+    {
+        comments,
+        errorFileRead,
+        errorInsertInDB,
+        errorPerformanceMetod,
+        errorInicializationMetod,
+        StopedMetodOnDemand
+    }
+    public static string ToString(GateError e)
+    {
+        string result = "";
+        switch (e)
+        {
+            case GateError.comments:
+                result = string.Empty; break;
+            case GateError.errorFileRead:
+                result = "Ошибка чтения файла"; break;
+            case GateError.errorInsertInDB:
+                result = "Ошибка добавления записи в БД"; break;
+            case GateError.errorPerformanceMetod:
+                result = "Ошибка в процессе выполнения метода"; break;
+            case GateError.errorInicializationMetod:
+                result = "Ошибка инициализации метода"; break;
+            case GateError.StopedMetodOnDemand:
+                result = "Метод остановлен по требованию"; break;
+        }
+        return result;
+    }
+}
